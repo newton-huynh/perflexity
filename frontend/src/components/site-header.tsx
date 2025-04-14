5f39936e30ae49e9207d6e5341e89f5c3eed30e3
Newton Huynh <newton.thieu@gmail.com>
2025-04-13 19:30:06 -0500

    Fix toggle button

--- frontend/src/components/site-header.tsx
+++ frontend/src/components/site-header.tsx
@@ -23,7 +23,7 @@ export function SiteHeader() {
     <header className="bg-background fixed top-0 z-50 flex w-full items-center border-b">
       <div className="flex h-(--header-height) w-full items-center justify-between gap-2 px-4">
         <Button
-          className="h-8 w-8 flex-1"
+          className="h-8 w-8 flex-1 max-w-20"
           variant="ghost"
           size="icon"
           onClick={toggleSidebar}
